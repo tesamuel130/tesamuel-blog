326d07eed35fcb1b4a74820a07a5f7a609c8b0dc
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-04-02 13:39:50 -0800

    second update commit

--- index.js
+++ index.js
@@ -22,4 +22,37 @@ function rotate(e){
     e.target.classList.add('rotate');
 }
 
-// ----------------------------
+// ---------------------------- email messaging------------------------------//
+
+function sendMessage() {
+    var params = {
+        from_name: document.getElementById("sender_name").value,
+        email_id: document.getElementById("sender_add").value,
+        message: document.getElementById("message").value + " " + document.getElementById("phone_no").value
+    };
+
+    var serviceID = "service_klktxrn"    // Correct the variable name if necessary
+    var templateID = "template_9z83kbv"  // Correct the variable name if necessary
+    var api_key = "8L8mQyyKKi5jJYPzB"    // Correct the variable name if necessary
+
+    var data = {
+        service_id: serviceID,
+        template_id: templateID,
+        user_id: api_key,
+        template_params: params
+    };
+
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error.text);
+    });
+
+    alert("Success Fully Sended")
+}
